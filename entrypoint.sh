4c639a147a466f0b0f7c2d86630edbfbc573aa9d
Kyle Gordon <kyle@lodge.glasgownet.com>
2020-07-05 23:20:22 +0100

    Keep Debian packaging happy

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,7 @@ rm -rf /build/*
 BUILD_VERSION=$(git rev-parse --short HEAD)
 BUILD_VERSION_TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
 git checkout ${BUILD_VERSION}
+DEBIAN_BUILD_VERSION=${MAJORVER}-${BUILD_VERSION}
 
 # Configure, make, make install
 mkdir /aasdk_build; cd /aasdk_build
@@ -26,11 +27,11 @@ mkdir -p /build/DEBIAN
 cp /src/resources/control.in /build/DEBIAN/control
 
 # Fill in the information in the control file
-sed -i "s/__VERSION__/${BUILD_VERSION:1}/g" /build/DEBIAN/control
+sed -i "s/__VERSION__/${DEBIAN_BUILD_VERSION}/g" /build/DEBIAN/control
 sed -i "s/__FILESIZE__/${INSTALL_SIZE}/g" /build/DEBIAN/control
 
 # Build our Debian package
 fakeroot dpkg-deb -b "/build"
 
 # Move it to release
-mv /build.deb /release/aasdk-${BUILD_VERSION}-amd64.deb
+mv /build.deb /release/aasdk-${DEBIAN_BUILD_VERSION}-${ARCH}.deb
