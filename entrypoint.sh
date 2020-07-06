dd9e316f447c052944eb36029497b6b5e9fbdd0c
Kyle Gordon <kyle@lodge.glasgownet.com>
2020-07-06 16:31:06 +0100

    Filter valid architectures

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 ARCH=$1
 MAJORVER=0
+
+if [ -z "$ARCH" ]
+then
+    echo "Please supply a target architecture to build"
+    echo "Choose from 'amd64', 'armhf'"
+    exit
+else
+    if [ "$ARCH" != "amd64" ] && [ "$ARCH" != "armhf" ]
+    then
+        echo "Invalid architecture requested"
+        exit
+    fi
+fi
+
 echo "Now building within docker for $ARCH"
 
 # Clear out the /build directory
